c009f9c2df89ecd7d5d97f1dd75eaea548beb035
Feilan Jiang <fjiang120@gmail.com>
2017-12-10 12:51:57 -0500

    create SessionComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SessionComponent } from './session/session.component';
 
 import { SortablejsModule } from 'angular-sortablejs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SessionComponent
   ],
   imports: [
     BrowserModule,
