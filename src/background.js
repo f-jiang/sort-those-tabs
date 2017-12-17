8743fd47775c1e04e2c4de1f8e60aea46f431662
Feilan Jiang <fjiang120@gmail.com>
2017-12-16 23:27:07 -0500

    prevent multiple extension tabs per window

--- src/background.js
+++ src/background.js
@@ -1,5 +1,13 @@
 (function() {
   chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({ 'url': chrome.extension.getURL('index.html') });
+    chrome.tabs.query({ 'title': 'SortThoseTabs', 'currentWindow': true }, function(result) {
+      // Open the extension in a new tab if it hasn't already been opened in the current window
+      if (result.length === 0) {
+        chrome.tabs.create({ 'url': chrome.extension.getURL('index.html') });
+      // Otherwise, activate the current window's pre-existing extension tab
+      } else {
+        chrome.tabs.update(result[0].id, { 'active': true }); // Assumption: tab.id property has been defined
+      }
+    });
   });
 })();
