64a1df8cfc2477f6d84ba19df8be8e2546e3df14
Feilan Jiang <fjiang120@gmail.com>
2018-03-25 22:02:04 -0400

    add tab closing stage to session diff algorithm

--- src/app/windows.service.ts
+++ src/app/windows.service.ts
@@ -43,10 +43,11 @@ export class WindowsService {
     const windowsToRemove_ids: Set<number> = new Set(Array.from(originalWindowIds)
       .filter(winId => !editedWindowIds.has(winId)));
 
-    const newWindows: chrome.windows.Window[] = editedWindows.filter(win => newWindows_ids.has(win.id));
+    const allDetachedTabs_ids: Set<number> = new Set();
 
     // 1. create the windows consisting of detached tabs
 
+    const newWindows: chrome.windows.Window[] = editedWindows.filter(win => newWindows_ids.has(win.id));
     for (const newWindow of newWindows) {
       const tabsToDetach_ids: number[] = newWindow.tabs.map(tab => tab.id);
       const result: chrome.windows.Window = await chromep.windows.create({ tabId: tabsToDetach_ids[0] });
@@ -54,6 +55,10 @@ export class WindowsService {
       // move additional detached tabs to new window
       if (tabsToDetach_ids.length > 1) {
         await chromep.tabs.move(tabsToDetach_ids.slice(1, tabsToDetach_ids.length), {windowId: result.id, index: -1});
+      }
+
+      for (const tabId of tabsToDetach_ids) {
+        allDetachedTabs_ids.add(tabId);
       }
     }
 
@@ -66,17 +71,24 @@ export class WindowsService {
       .filter(win => windowsInCommon_ids.has(win.id));
 
     // maps of above windows' tab id arrays; key = windowId, value = set of tab ids
-    const windowsInCommon_tabIds_original: { [windowId: number]: Set<number> } = { };
-    const windowsInCommon_tabIds_edited: { [windowId: number]: Set<number> } = { };
+    const windowsInCommon_tabIdsMap_original: Map<number, Set<number>> = new Map();
+    const windowsInCommon_tabIdsMap_edited: Map<number, Set<number>> = new Map();
+    const windowsInCommon_tabIdsSet_edited: Set<number> = new Set();
 
-    // populate tab id maps
+    // populate tab id maps and sets
 
     for (const window of windowsInCommon_original) {
-      windowsInCommon_tabIds_original[window.id] = new Set(window.tabs.map(tab => tab.id));
+      windowsInCommon_tabIdsMap_original.set(window.id, new Set(window.tabs.map(tab => tab.id)));
     }
 
     for (const window of windowsInCommon_edited) {
-      windowsInCommon_tabIds_edited[window.id] = new Set(window.tabs.map(tab => tab.id));
+      const tabIds: number[] = window.tabs.map(tab => tab.id);
+
+      windowsInCommon_tabIdsMap_edited.set(window.id, new Set(tabIds));
+
+      for (const tabId of tabIds) {
+        windowsInCommon_tabIdsSet_edited.add(tabId);
+      }
     }
 
     // if a certain window has all its original tabs moved to other windows via the extension, a temporary tab will be
@@ -87,14 +99,14 @@ export class WindowsService {
 
     for (const windowInCommon_id of Array.from(windowsInCommon_ids)) {
       // set of ids of tabs present in the window, before and after modification
-      const originalTabIds: Set<number> = windowsInCommon_tabIds_original[windowInCommon_id];
-      const editedTabIds: Set<number> = windowsInCommon_tabIds_edited[windowInCommon_id];
+      const originalTabIds: Set<number> = windowsInCommon_tabIdsMap_original.get(windowInCommon_id);
+      const editedTabIds: Set<number> = windowsInCommon_tabIdsMap_edited.get(windowInCommon_id);
 
       // move transferred tabs into the window
+
       const tabsToMove_ids: number[] = Array.from(editedTabIds).filter(id => !originalTabIds.has(id));
 
-      for (const windowId of Object.keys(windowsInCommon_tabIds_original)) {
-        const tabIds: Set<number> = windowsInCommon_tabIds_original[windowId];
+      for (const [windowId, tabIds] of windowsInCommon_tabIdsMap_original) {
         const intersection: number[] = tabsToMove_ids.filter(id => tabIds.has(id));
 
         // if tabIds is a subset of tabsToMove_ids, create a temporary tab
@@ -107,6 +119,19 @@ export class WindowsService {
       if (tabsToMove_ids.length > 0) {
         await chromep.tabs.move(tabsToMove_ids, { index: -1, windowId: windowInCommon_id });
       }
+
+      // TODO untested
+      // close tabs to be removed from window
+
+      // tabs to be removed...
+      const tabsToRemove_ids: number[] = Array.from(originalTabIds).filter(
+        id => !editedTabIds.has(id) &&          // ...can't be in the window after editing via the extension
+          !windowsInCommon_tabIdsSet_edited.has(id) &&   // ...can't have been transferred into another unclosed window
+          !allDetachedTabs_ids.has(id));                 // ...can't be one of the detached tabs
+
+      if (tabsToRemove_ids.length > 0) {
+        await chromep.tabs.remove(tabsToRemove_ids);
+      }
     }
 
     if (placeholderTabs_ids.length > 0) {
@@ -116,7 +141,7 @@ export class WindowsService {
     // 2.1. sort the tabs in each of the windows from part 2
 
     for (const windowInCommon_id of Array.from(windowsInCommon_ids)) {
-      const targetSortOrder: number[] = Array.from(windowsInCommon_tabIds_edited[windowInCommon_id]);
+      const targetSortOrder: number[] = Array.from(windowsInCommon_tabIdsMap_edited.get(windowInCommon_id));
 
       const editedWindow: chrome.windows.Window = await chromep.windows.get(windowInCommon_id, { populate: true });
       const currentTabIds: number[] = editedWindow.tabs.map(tab => tab.id);
