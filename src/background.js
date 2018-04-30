1dea20bfdb745596e744c0575ff2b2f536c5dc4e
Feilan Jiang <fjiang120@gmail.com>
2018-04-29 23:59:41 -0400

    fully update tab title

--- src/background.js
+++ src/background.js
@@ -1,6 +1,8 @@
 (function() {
   chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.query({ 'title': 'SortThoseTabs', 'currentWindow': true }, function(result) {
+    // title 'Sort those Tabs' defined in index.html
+    chrome.tabs.query({ 'title': 'Sort those Tabs', 'currentWindow': true }, function(result) {
+      debugger;
       // Open the extension in a new tab if it hasn't already been opened in the current window
       if (result.length === 0) {
         chrome.tabs.create({ 'url': chrome.extension.getURL('index.html') });
